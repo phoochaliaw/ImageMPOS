7f3dee3c8f8cf3b7a9d141233d71299b65bad0cd
Tanakorn Phoochaliaw <tanakorn@digio.co.th>
2016-12-22 16:14:53 +0700

    Fixed load image.

--- ImageMPOS/Classes/ImageMPOS.swift
+++ ImageMPOS/Classes/ImageMPOS.swift
@@ -11,7 +11,15 @@ import Foundation
 public class ImageMPOS {
     public class func imageName(name: String) -> UIImage {
         
-        let bundle = Bundle(for: self)
-        return UIImage(named: name, in: bundle,compatibleWith: nil)!
+//        let bundle = Bundle(for: self)
+//        return UIImage(named: name, in: bundle,compatibleWith: nil)!
+        
+        let podBundle = Bundle(for: ImageMPOS.self)
+//        print(podBundle)
+        if let url = podBundle.url(forResource: "ImageMPOS", withExtension: "bundle") {
+            let bundle = Bundle(url: url)
+            return UIImage(named: name, in: bundle, compatibleWith: nil)!
+        }
+        return UIImage()
     }
 }
